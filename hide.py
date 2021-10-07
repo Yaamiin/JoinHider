9b765ce3e9cc7a19987652110d924068af90ea39
TechincalHunter <92113924+TechnicalHunter@users.noreply.github.com>
2021-10-08 01:54:50 +0530

    Update hide.py

--- hide.py
+++ hide.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 12345))
 
 API_HASH = os.environ.get("API_HASH", "")
-app = Client(
+bot = Client(
         "hide",
         bot_token=BOT_TOKEN,api_hash=API_HASH,
             api_id=API_ID
