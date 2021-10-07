d7dd2de5d319a2d0358ba70802db79535d35ec81
TechincalHunter <92113924+TechnicalHunter@users.noreply.github.com>
2021-10-08 02:27:18 +0530

    Update hide.py

--- hide.py
+++ hide.py
@@ -1,6 +1,7 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
+from pyrogram import Button
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
@@ -12,6 +13,16 @@ bot = Client(
         bot_token=BOT_TOKEN,api_hash=API_HASH,
             api_id=API_ID
     )
+
+@bot.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("**Im JoinHider**,\n\nAdd me as an admin your group. Made In India 🇮🇳",
+                    buttons=(
+                      [Button.url('Add Me', 'https://t.me/JOlNHlDERBOT?startgroup=true'),
+                      Button.url('📜 Source', 'https://github.com/TechnicalHunter/JoinHider/tree/Tanaji')]
+                    ),
+                    link_preview=False
+                   )
 
 @bot.on_message(filters.new_chat_members)
 async def welcome(bot,message):
