cc06801e813bd6a5c8e626a8e467320ded54da12
TechincalHunter <92113924+TechnicalHunter@users.noreply.github.com>
2021-10-08 01:37:38 +0530

    Update hide.py

--- hide.py
+++ hide.py
@@ -4,9 +4,9 @@ from pyrogram.types import Message, User
 
 bot = Client(
     "BotNameHere",
-     bot_token = os.environ["BOT_TOKEN"],
-     api_id = int(os.environ["API_ID"]),
-     api_hash = os.environ["API_HASH"]
+     api_id = int(os.environ.get("APP_ID"))
+     api_hash = os.environ.get("API_HASH")
+     bot_token = os.environ.get("BOT_TOKEN")
 )
 
 @bot.on_message(filters.new_chat_members)
