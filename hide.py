90f4ba2c47fb0efc2a2840bc6721ba6bc8989707
Tanaji <92113924+Tanaji56@users.noreply.github.com>
2021-10-15 12:33:23 +0530

    Update hide.py

--- hide.py
+++ hide.py
@@ -15,7 +15,7 @@ bot = Client(
 
 @bot.on_message(filters.command('start'))
 async def start(bot, message):
-	text = 'Hey, Im a Join Hider Bot'
+	text = 'Hey, Im a Join Hider Bot\n\n I Can Delete A Member joined Message.\n\n Add Me To Your Group And Give permission Of delete message.\n\n If You Want To Make Your Own Bot Here Is The [Repositry](https://github.com/Tanaji56/JoinHider).'
 	await message.reply(text, quote=True)
 
 @bot.on_message(filters.new_chat_members)
