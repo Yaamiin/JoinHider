4a537c4e6fd05c722cd260da150376a45b77b000
Mr.Tanaji <92113924+Mr-Tanaji@users.noreply.github.com>
2021-10-26 22:34:32 +0530

    Update hide.py

--- hide.py
+++ hide.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 8709133))
+API_ID = int(os.environ.get("API_ID", 5891777))
 API_HASH = os.environ.get("API_HASH", "")
 
 bot = Client(
@@ -25,5 +25,9 @@ async def welcome(bot, message):
 @bot.on_message(filters.left_chat_member)
 async def goodbye(bot, message):
 	await message.delete()	
+
+@bot.on_message(filters.member_invited)
+async def welcome(bot, message):
+        await message.delete()
 	
 bot.run()
