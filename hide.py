06914f3e6e10a262dafe163362b2b12c13c0c0ea
Mr.Tanaji <92113924+Mr-Tanaji@users.noreply.github.com>
2021-10-29 18:20:58 +0530

    Done

--- hide.py
+++ hide.py
@@ -3,8 +3,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 5891777))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = 5891777
+API_HASH = '64fa91f5fafd43a3b9dc504f0e2a4d70'
 
 bot = Client(
         "hide",
@@ -15,7 +15,7 @@ bot = Client(
 
 @bot.on_message(filters.command('start'))
 async def start(bot, message):
-	text = 'Hey, Im a Join Hider Bot\n\n I Can Delete A Member joined Message.\n\n Add Me To Your Group And Give permission Of delete message.'
+	text = 'Hey, Im a Join Hider Bot\n\n I Can Delete A Member joined Message.\n\n Add Me To Your Group And Give permission Of delete message.\n\n ©️ @sillybots'
 	await message.reply(text, quote=True)
 
 @bot.on_message(filters.new_chat_members)
